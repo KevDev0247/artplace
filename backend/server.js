58d4053e85a15c50f49dadc26bec608e215757e2
Kevin <zhijunwang2002@gmail.com>
2022-08-02 20:22:39 -0400

    successfully connected to database

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,14 @@
 const app = require("./app");
-
-const dotenv = require("dotenv")
+const dotenv = require("dotenv");
+const connectDatabase = require("./db/Database.js");
 
 // config
 dotenv.config({
     path: "backend/config/.env"
 })
+
+// connect database
+connectDatabase();
 
 // create server
 const server = app.listen(process.env.PORT, () => {
