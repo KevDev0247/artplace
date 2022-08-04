87357961efc4dca45558c4cb61930b8468949502
Kevin <zhijunwang2002@gmail.com>
2022-08-04 11:51:18 -0400

    Add internal error handling

--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,16 @@ const app = require("./app");
 const dotenv = require("dotenv");
 const connectDatabase = require("./db/Database.js");
 
+
+process.on("uncaughtException", (err) => {
+    console.log(`Error" ${err.message}`);
+    console.log(`Shutting down the server for handling uncaught exception`);
+});
+
 // config
 dotenv.config({
     path: "backend/config/.env"
-})
+});
 
 // connect database
 connectDatabase();
@@ -13,4 +19,13 @@ connectDatabase();
 // create server
 const server = app.listen(process.env.PORT, () => {
     console.log(`Server is working on http://localhost:${process.env.PORT}`)
+});
+
+// unhandled promise rejection
+process.on("unhandledRejection", (err) => {
+    console.log(`Shutting down the server for ${err.message}`);
+    console.log(`Shutting down the server due to unhandled promise rejection`)
+    server.close(() => {
+        process.exit(1);
+    })
 });
