11409a2e0993be333ec5ebdfeb5a8b00ed27ed67
Kevin <zhijunwang2002@gmail.com>
2022-08-21 17:18:29 -0400

    Created login function

--- backend/controller/UserController.js
+++ backend/controller/UserController.js
@@ -1,9 +1,9 @@
 const User = require("../models/UserModel");
 const ErrorHandler = require("../utils/ErrorHandler");
 const catchAsyncError = require("../middleware/catchAsyncErrors");
+const catchAsyncErrors = require("../middleware/catchAsyncErrors");
 
 // register user
-
 exports.createUser = catchAsyncError(async (req, res, next) => {
     const {name, email, password} = req.body;
 
@@ -16,6 +16,30 @@ exports.createUser = catchAsyncError(async (req, res, next) => {
             url: "http://test.com"
         }
     });
+    const token = user.getJwtToken();
+
+    res.status(201).json({
+        success: true,
+        token
+    });
+});
+
+// login user
+exports.loginUser = catchAsyncErrors(async (req, res, next) => {
+    const {email, password} = req.body
+    if (!email || !password) {
+        return next(new ErrorHandler("Please enter your email and password"), 400);
+    }
+    const user = await User.findOne({email}).select("+password");
+
+    if (!user) {
+        return next(new ErrorHandler("User is not found with this email & password", 401));
+    }
+
+    const isPasswordMatched = await user.comparePassword(password);
+    if (!isPasswordMatched) {
+        return next(new ErrorHandler("User is not found with this email & password", 401));
+    }
     const token = user.getJwtToken();
 
     res.status(201).json({
--- backend/routes/UserRoute.js
+++ backend/routes/UserRoute.js
@@ -1,7 +1,9 @@
 const express = require("express");
-const { createUser } = require("../controller/UserController");
+const { createUser, loginUser } = require("../controller/UserController");
 const router = express.Router();
 
 router.route("/registration").post(createUser);
+
+router.route("/login").post(loginUser);
 
 module.exports = router;
