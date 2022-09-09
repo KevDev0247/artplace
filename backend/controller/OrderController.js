bbdd5e5ab5f61e1cfcf62bc1600d3f55ecd5ce07
Kevin <zhijunwang2002@gmail.com>
2022-09-09 13:51:30 -0400

    Developed get single order api

--- backend/controller/OrderController.js
+++ backend/controller/OrderController.js
@@ -31,4 +31,21 @@ exports.createOrder = catchAsyncErrors(async (req, res, next) => {
         success: true,
         order
     })
+});
+
+// Get single order
+exports.getSingleOrder = catchAsyncErrors(async(req, res, next) => {
+    const order = await Order.findById(req.params.id).populate(
+        "user",
+        "name email"
+    );
+
+    if (!order) {
+        return next(new ErrorHandler("Order items not found with this id", 404))
+    };
+
+    res.status(200).json({
+        success: true,
+        order
+    })
 })
--- backend/routes/OrderRoute.js
+++ backend/routes/OrderRoute.js
@@ -1,8 +1,9 @@
 const express = require("express");
-const { createOrder } = require("../controller/OrderController");
+const { createOrder, getSingleOrder } = require("../controller/OrderController");
 const { isAuthenticatedUser, authorizeRoles } = require("../middleware/auth")
 const router = express.Router();
 
 router.route("/order/new").post(isAuthenticatedUser, createOrder);
+router.route("/order/:id").get(isAuthenticatedUser, getSingleOrder);
 
 module.exports = router;
